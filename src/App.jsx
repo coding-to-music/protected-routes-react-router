8c36a867fd24badbf8b91769d7666cb6f0bbf639
coding-to-music <connors.tom@gmail.com>
2023-09-03 23:11:02 -0500

    Build

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,70 @@
-import { useState } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { AdminPage, ConfigPage, DashboardPage, HomePage, LandingPage } from "./pages"
-import ProtectedRoute from "./components/ProtectedRoute"
-import Nav from "./components/layout/Nav"
+import {
+  AdminPage,
+  ConfigPage,
+  DashboardPage,
+  HomePage,
+  LandingPage,
+} from "./pages";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Nav from "./components/layout/Nav";
+import { Analytics } from "@vercel/analytics/react";
 
-import "./App.css"
+import "./App.css";
 
 function App() {
-	const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
-	return (
-		<BrowserRouter>
-			<Nav user={user} setUser={setUser} />
+  return (
+    <BrowserRouter>
+      <Nav user={user} setUser={setUser} />
 
-			<Routes>
-				<Route index element={<LandingPage />} />
-				<Route path='/landing' element={<LandingPage />} />
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="/landing" element={<LandingPage />} />
 
-				<Route element={<ProtectedRoute isAllowed={!!user} />}>
-					<Route path='/home' element={<HomePage />} />
-					<Route
-						path='/dashboard'
-						element={
-							<ProtectedRoute isAllowed={!!user && user.permissions.includes("dashboard")} redirectTo='/home'>
-								<DashboardPage />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path='/admin'
-						element={
-							<ProtectedRoute isAllowed={!!user && user.roles.includes("admin")} redirectTo='/home'>
-								<AdminPage />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path='/config'
-						element={
-							<ProtectedRoute isAllowed={!!user && user.roles.includes("admin")} redirectTo='/home'>
-								<ConfigPage />
-							</ProtectedRoute>
-						}
-					/>
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	)
+        <Route element={<ProtectedRoute isAllowed={!!user} />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute
+                isAllowed={!!user && user.permissions.includes("dashboard")}
+                redirectTo="/home"
+              >
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute
+                isAllowed={!!user && user.roles.includes("admin")}
+                redirectTo="/home"
+              >
+                <AdminPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/config"
+            element={
+              <ProtectedRoute
+                isAllowed={!!user && user.roles.includes("admin")}
+                redirectTo="/home"
+              >
+                <ConfigPage />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+      <Analytics />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
